ed31b96aa3a839f345c695ffaad96a97b326650b
Vika <makosch2001@yandex.ru>
2024-06-14 17:11:46 +0300

    My first commit

---
+++ sender_stand_request.py
@@ -0,0 +1,15 @@
+import requests
+import configuration
+import data
+
+def get_new_user(body):
+
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,
+                         json=body,
+                         headers=data.headers)
+
+
+response = get_new_user(data.user_body)
+
+print(response.status_code)
+print(response.json())
